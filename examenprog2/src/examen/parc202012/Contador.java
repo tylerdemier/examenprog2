8b6915cf6d049dd01f362f882807dfcbcab11d4e
Tyler de Mier <tylerdemier@opendeusto.es>
2020-12-10 22:29:21 +0100

    examen2

--- examenprog2/src/examen/parc202012/Contador.java
+++ examenprog2/src/examen/parc202012/Contador.java
@@ -1,33 +1,52 @@
 package examen.parc202012;
 
-/** Clase para gestionar contadores
+/**
+ * Clase para gestionar contadores
+ * 
  * @author andoni.eguiluz @ ingenieria.deusto.es
  */
 public class Contador {
-	
-	private int cont = 0;  // Contador entero
-	
-	/** Inicializa un contador a cero
+
+	private int cont = 0; // Contador entero
+
+	/**
+	 * Inicializa un contador a cero
 	 */
 	public Contador() {
 	}
-	/** Inicializa un contador con el valor indicado
-	 * @param valor	Valor de inicio del contador
+
+	/**
+	 * Inicializa un contador con el valor indicado
+	 * 
+	 * @param valor Valor de inicio del contador
 	 */
-	public Contador( int valor ) {
+	public Contador(int valor) {
 		cont = valor;
 	}
-	/** Devuelve el valor del contador
-	 * @return	Valor actual del contador
+
+	/**
+	 * Devuelve el valor del contador
+	 * 
+	 * @return Valor actual del contador
 	 */
-	public int get() { return cont; }
-	/** Incrementa el contador
+	public int get() {
+		return cont;
+	}
+
+	/**
+	 * Incrementa el contador
 	 */
-	public void inc() { cont++; }
-	
-	public void inc( int inc ) { cont += inc; }
-	
+	public void inc() {
+		cont++;
+	}
+
+	public void inc(int inc) {
+		cont += inc;
+	}
+
 	@Override
-	public String toString() { return "" + cont; }
-	
+	public String toString() {
+		return "" + cont;
+	}
+
 }
--- examenprog2/src/examen/parc202012/VentanaGeneral.java
+++ examenprog2/src/examen/parc202012/VentanaGeneral.java
@@ -9,90 +9,101 @@ import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class VentanaGeneral extends JFrame {
-	
-	public static Color COLOR_GRIS_CLARITO = new Color( 220, 220, 220 );
-	
+
+	public static Color COLOR_GRIS_CLARITO = new Color(220, 220, 220);
+
 	private JDesktopPane desktop;
-	private JLabel lMensaje = new JLabel( " " );
+	private JLabel lMensaje = new JLabel(" ");
 	private JMenu menuVentanas;
 	private JMenu menuAcciones;
 	private ArrayList<JInternalFrame> misSubventanas;
 	private Runnable runEnCierre;
-	
+
 	public VentanaGeneral() {
 		misSubventanas = new ArrayList<>();
 		// Configuración general
-		setTitle( "Ventana General" );
-		setSize( 1200, 800 ); // Tamaño por defecto
-		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		setTitle("Ventana General");
+		setSize(1200, 800); // Tamaño por defecto
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		// Creación de componentes y contenedores
 		desktop = new JDesktopPane();
-		add( desktop, BorderLayout.CENTER );
+		add(desktop, BorderLayout.CENTER);
 		// setContentPane( desktop );
-		lMensaje.setOpaque( true );
-		add( lMensaje, BorderLayout.SOUTH );
+		lMensaje.setOpaque(true);
+		add(lMensaje, BorderLayout.SOUTH);
 		// Eventos
-		addWindowListener( new WindowAdapter() {
+		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosed(WindowEvent e) {
-				if (runEnCierre!=null) runEnCierre.run();
+				if (runEnCierre != null)
+					runEnCierre.run();
 			}
 		});
 		// Menú y eventos
 		JMenuBar menuBar = new JMenuBar();
-		menuVentanas = new JMenu( "Ventanas" ); menuVentanas.setMnemonic( KeyEvent.VK_V );
-		menuBar.add( menuVentanas );
-		menuAcciones = new JMenu( "Acciones" ); menuAcciones.setMnemonic( KeyEvent.VK_A );
-		menuBar.add( menuAcciones );
-		setJMenuBar( menuBar );
+		menuVentanas = new JMenu("Ventanas");
+		menuVentanas.setMnemonic(KeyEvent.VK_V);
+		menuBar.add(menuVentanas);
+		menuAcciones = new JMenu("Acciones");
+		menuAcciones.setMnemonic(KeyEvent.VK_A);
+		menuBar.add(menuAcciones);
+		setJMenuBar(menuBar);
 	}
-	
-	public void setEnCierre( Runnable runnable ) {
+
+	public void setEnCierre(Runnable runnable) {
 		runEnCierre = runnable;
 	}
-	
-		private ActionListener alMenu = new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String vent = e.getActionCommand();
-				for (JInternalFrame vd : misSubventanas) if (vd.getName().equals( vent )) { vd.setVisible( true ); vd.moveToFront(); return; }
-			}
-		};
-	public void addVentanaInterna( JInternalFrame f, String codVentana ) {
-		desktop.add( f );
-		JMenuItem menuItem = new JMenuItem( codVentana ); 
-		menuItem.setActionCommand( codVentana );
-		menuItem.addActionListener( alMenu );
-		menuVentanas.add( menuItem );	
-		misSubventanas.add( f );
-		f.setName( codVentana );
+
+	private ActionListener alMenu = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String vent = e.getActionCommand();
+			for (JInternalFrame vd : misSubventanas)
+				if (vd.getName().equals(vent)) {
+					vd.setVisible(true);
+					vd.moveToFront();
+					return;
+				}
+		}
+	};
+
+	public void addVentanaInterna(JInternalFrame f, String codVentana) {
+		desktop.add(f);
+		JMenuItem menuItem = new JMenuItem(codVentana);
+		menuItem.setActionCommand(codVentana);
+		menuItem.addActionListener(alMenu);
+		menuVentanas.add(menuItem);
+		misSubventanas.add(f);
+		f.setName(codVentana);
 	}
-	
-	public void setMensaje( String mens, Color... colorFondo ) {
-		Color fondo = (colorFondo.length>0) ? colorFondo[0] : COLOR_GRIS_CLARITO; 
-		if (mens==null || mens.isEmpty()) mens = " ";
-		lMensaje.setText( mens );
-		lMensaje.setBackground( fondo );
+
+	public void setMensaje(String mens, Color... colorFondo) {
+		Color fondo = (colorFondo.length > 0) ? colorFondo[0] : COLOR_GRIS_CLARITO;
+		if (mens == null || mens.isEmpty())
+			mens = " ";
+		lMensaje.setText(mens);
+		lMensaje.setBackground(fondo);
 	}
-	
-	public void setMensajeSinCambioColor( String mens ) {
-		if (mens==null || mens.isEmpty()) mens = " ";
-		lMensaje.setText( mens );
+
+	public void setMensajeSinCambioColor(String mens) {
+		if (mens == null || mens.isEmpty())
+			mens = " ";
+		lMensaje.setText(mens);
 	}
-	
-	public void addMenuAccion( String textoMenu, ActionListener accion ) {
-		JMenuItem menuItem = new JMenuItem( textoMenu );
-		menuItem.setActionCommand( textoMenu );
-		menuItem.addActionListener( accion );
-		menuAcciones.add( menuItem );
+
+	public void addMenuAccion(String textoMenu, ActionListener accion) {
+		JMenuItem menuItem = new JMenuItem(textoMenu);
+		menuItem.setActionCommand(textoMenu);
+		menuItem.addActionListener(accion);
+		menuAcciones.add(menuItem);
 	}
-	
+
 	public ArrayList<JInternalFrame> getSubventanas() {
 		return misSubventanas;
 	}
-	
+
 	public JDesktopPane getJDesktopPane() {
 		return desktop;
 	}
-	
+
 }
